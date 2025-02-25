17114eff85fb5192e1d7fe083ce0e1976bda3750
Amanuel Wubishet <162797908+BIGANUEL@users.noreply.github.com>
2025-02-25 14:31:59 +0300

    Update vehicle.py

--- Week3/vehicle.py
+++ Week3/vehicle.py
@@ -1,1 +1,7 @@
+class Vehicle:
+    def __init__(self, make, model):
+        self._make = make  
+        self._model = model 
 
+    def describe(self):
+        return f"Vehicle: {self._make} {self._model}"
